37ab8e5711e3681c4537f7f71d3bd2be1d110eaa
Aamir Bhatti <aamir.shahzad@global.ntt>
2022-06-02 21:40:45 +0200

    Modify first.tf

--- first.tf
+++ first.tf
@@ -3,8 +3,7 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-#  "tf_course" = friendly name of the bucket
 resource "aws_s3_bucket" "tf_course" {
-  bucket = "tf-course-aa-02062022" # unique name alphanumeric and - possible only
+  bucket = "tf-course-aa-02062022" 
   acl 	 = "private"
 }
